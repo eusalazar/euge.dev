b26e245811b20bbc40ae34edc07187f3f20513d5
eusalazar <eugeniasalazar745@gmail.com>
2025-01-19 15:50:33 -0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/euge.dev/', // This should match your repository name
+  base: '/', // This should match your repository name
 })
