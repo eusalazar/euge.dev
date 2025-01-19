5790cb02d94b9f1c29f23e664116710efd4d5932
eusalazar <eugeniasalazar745@gmail.com>
2025-01-19 17:29:19 -0300

    file renaming

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Change the base URL to match your repository name
   base: '/',
   build: {
     outDir: 'dist',
-    // Ensure the assets are properly named
     assetsDir: 'assets',
-    // Generate source maps for debugging
-    sourcemap: true
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+      },
+      output: {
+        entryFileNames: `assets/[name].[hash].js`,
+        chunkFileNames: `assets/[name].[hash].js`,
+        assetFileNames: `assets/[name].[hash].[ext]`
+      }
+    }
   }
 })
